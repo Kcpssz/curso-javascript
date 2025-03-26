206658399d96f8f8af4a736f0cf6f5dcf53778a2
Kcpssz <devKauaCavalcanti@gmail.com>
2025-03-26 11:24:37 -0300

    pasta para organizar os cursos

--- Curso/aula1/atividades/exer5.js
+++
@@ -1,6 +0,0 @@
-var teste = 'melhor do mundo';
-if(1 > 0){
-var teste2 = 'pior do mundo';
-    console.log('kauã voce é o: ' + teste)
-}
-console.log('kauã voce é o: ' + teste2);
--- Curso/aula1/escopo.js
+++
@@ -1,9 +0,0 @@
-let estudante;
-
-if(1 > 0) {
-    estudante = 'Kauã';
-    console.log(estudante);
-}
-//bloco de codigo é tudo que esta dentro de {}
-// estudante = 'Pedro'
-console.log(estudante);
